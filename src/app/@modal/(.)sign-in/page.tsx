a5bf28189e5a7c524869f7fcce2cf09cb0e32376
Kwanchai <alphezelphaz@gmail.com>
2023-06-05 16:24:32 +0700

    intercept+parallel with clerk modal done

---
+++ src/app/@modal/(.)sign-in/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+import Modal from "@/components/modal";
+
+export default function InterceptedSignInPage() {
+  return (
+    <Modal>
+      <SignIn afterSignInUrl="/" />
+    </Modal>
+  );
+}
